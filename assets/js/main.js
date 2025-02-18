beadee29c7beb5ffeaca2a1aa271f951dda9231c
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-02-18 18:07:38 -0300

    fit: portfolio create

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+import { menu } from "./menu.js";
+
+menu();
+
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
