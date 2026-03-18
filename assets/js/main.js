b49debda7334e5f58e798d382e02b8dded9850ab
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2026-03-18 20:36:58 -0300

    teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,27 @@ const swiper = new Swiper(".slider-wrapper", {
     },
   },
 });
+
+const btnDownload = document.getElementById("btn-download");
+
+if (btnDownload) {
+  btnDownload.addEventListener("click", () => {
+    // Seleciona o elemento que você quer transformar em PDF (neste caso, o <main> inteiro)
+    const element = document.querySelector("main");
+
+    // Configurações do PDF
+    const options = {
+      margin: 10, // Margem de 10mm
+      filename: "Curriculo_Ramon_Castro_Barbosa.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: {
+        scale: 2, // Melhora a resolução do PDF
+        useCORS: true // Permite carregar imagens externas ou fontes do Google
+      },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
+    };
+
+    // Gera o PDF
+    html2pdf().set(options).from(element).save();
+  });
+}
