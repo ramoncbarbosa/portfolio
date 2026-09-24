078b44cac5ae91336f2a15e8176dc6ceed142611
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2026-09-24 01:51:36 -0300

    feat: pequenos ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import { menu } from "./menu.js";
 import { skillsCarousel } from "./skills.js";
-import { i18n } from "./i18n.js";
+import { i18n } from "./i18n.js?v=profile-copy-refresh-9";
 
 menu();
 skillsCarousel();
